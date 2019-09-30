fd6f6082ce6fe4a98818927cef9d11b292e48afe
Rainon <hrtechnicien.assistance.info@gmail.com>
2019-09-30 09:12:59 +0200

    modifs css + js + html

--- javascript/script.js
+++ javascript/script.js
@@ -1,56 +1,70 @@
-_currentItems=null;
+
 /*random*/
-function _shuffle (array) {
-  for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+let _key = "array";
+let _currentItems = [];
+
+function _shuffle(array) {
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }
 function clickRandom() {
-var randomItem  = (function(allItems){
-  var _key = "array";
-  var _currentItems = [];
-  try {
-    _currentItems = JSON.parse(localStorage.getItem(_key) || "[]");
-  } catch (e) {
-    _currentItems = [];
-  }
-  if (!Array.isArray(_currentItems) || _currentItems.length === 0 ) {
-    console.log("resetting");
-    _currentItems = _shuffle(allItems.slice());
-  }
-  var _selectedItem = _currentItems.pop();
-  localStorage.setItem(_key, JSON.stringify(_currentItems));
-  return _selectedItem;
-})(["MOHAMED S.","MOHAMED K.","HERVÉ","SALIM","ANDRÉA","HADIBÉRÉ","YANNICK","AUDREY","SIKA","AMEL"]);
+  let randomItem = (function (allItems) {
+
+    try {
+      _currentItems = JSON.parse(localStorage.getItem(_key) || "[]");
+    } catch (e) {
+      _currentItems = [];
+    }
+    if (!Array.isArray(_currentItems) || _currentItems.length === 0) {
+      console.log("resetting");
+      _currentItems = _shuffle(allItems.slice());
+    }
+    let _selectedItem = _currentItems.pop();
+    localStorage.setItem(_key, JSON.stringify(_currentItems));
+    return _selectedItem;
+  })(["MOHAMED S.", "MOHAMED K.", "HERVÉ", "SALIM", "ANDRÉA", "HADIBÉRÉ", "YANNICK", "AUDREY", "SIKA", "AMEL"]);
 
 /*trigger btn*/
 document.getElementById("noms").innerHTML = randomItem;
-let element = document.getElementById("cellphone");
+let element = document.getElementById("cellphone"); 
 element.classList.add("motion");
+const displayNone = document.querySelector('.logoanim');
 setTimeout(function(){noms.style.filter = "blur()"; }, 3000);
 element.style.animation = 'motion';
+console.log(displayNone.style.visibility);
+displayNone.style.visibility = "hidden";
 
 
-/* trigger reflow */
+/* trigger reflow */ 
+noms.offsetHeight;
+element.offsetHeight; 
 element.style.animation = null; 
 noms.style.filter = null;
 
+
+
 }
-/*click*/ 
+/*click*/
 document.getElementById("callTOaction").addEventListener("click", () => {
   clickRandom();
-  
+
 });
 
-let form = document.querySelector('form');
+document.getElementById("callTOaction").addEventListener("mouseout", () => {
+  displayNone.style.visibility = "visible";
 
-form.addEventListener('submit', e =>{
+});
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const name = form.name.value;
+  let input = document.getElementById('name');
+  _currentItems.push(input.value);
+  console.log(_currentItems);
+});
 
-  name.value.push(_currentItems)
-})
