6cde7cf08164201bfbce1140f81376a06c1ccd3b
Rainon <hrtechnicien.assistance.info@gmail.com>
2019-09-30 11:52:15 +0200

    anim fd js

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,13 @@
 
+
+  
+
+
+
+
 /*random*/
 let _key = "array";
 let _currentItems = [];
-
 function _shuffle(array) {
   for (i = array.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
@@ -12,9 +17,15 @@ function _shuffle(array) {
   }
   return array;
 }
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let input = document.getElementById('name');
+  _currentItems.push(input.value);
+  console.log(_currentItems);
+});
 function clickRandom() {
   let randomItem = (function (allItems) {
-
     try {
       _currentItems = JSON.parse(localStorage.getItem(_key) || "[]");
     } catch (e) {
@@ -27,24 +38,36 @@ function clickRandom() {
     let _selectedItem = _currentItems.pop();
     localStorage.setItem(_key, JSON.stringify(_currentItems));
     return _selectedItem;
-  })(["MOHAMED S.", "MOHAMED K.", "HERVÉ", "SALIM", "ANDRÉA", "HADIBÉRÉ", "YANNICK", "AUDREY", "SIKA", "AMEL"]);
+  })(_currentItems);
+
 
 /*trigger btn*/
+const personnage1 =  document.querySelector('.container');
+personnage1.innerHTML += `<div class="slideoutX"><img src="../img/personnages.png" alt="personnages"></div>`;
+
+
 document.getElementById("noms").innerHTML = randomItem;
 let element = document.getElementById("cellphone"); 
 element.classList.add("motion");
-const displayNone = document.querySelector('.logoanim');
+let element2 = document.querySelector(".secouezmoi"); 
+element2.classList.add("motion2");
 setTimeout(function(){noms.style.filter = "blur()"; }, 3000);
 element.style.animation = 'motion';
+element2.style.animation = 'motion2';
+
+/*const displayNone = document.querySelector('.logoanim');
 console.log(displayNone.style.visibility);
-displayNone.style.visibility = "hidden";
+displayNone.style.visibility = "hidden";*/
 
 
 /* trigger reflow */ 
 noms.offsetHeight;
 element.offsetHeight; 
-element.style.animation = null; 
+element.style.animation = null;
+element2.offsetHeight; 
+element2.style.animation = null;  
 noms.style.filter = null;
+
 
 
 
@@ -55,16 +78,28 @@ document.getElementById("callTOaction").addEventListener("click", () => {
 
 });
 
-document.getElementById("callTOaction").addEventListener("mouseout", () => {
+/*document.getElementById("callTOaction").addEventListener("mouseout", () => {
   displayNone.style.visibility = "visible";
 
-});
-
+});*/
+let input = document.getElementById('name');
 var form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  let input = document.getElementById('name');
   _currentItems.push(input.value);
   console.log(_currentItems);
 });
+
+/*shake phone */
+let shakeEvent = new Shake({threshold: 15});
+shakeEvent.start();
+window.addEventListener('shake', function(){
+  clickRandom();
+
+}, false);
+
+//stop listening
+function stopShake(){
+    shakeEvent.stop();
+}
 
