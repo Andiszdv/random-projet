86efac7430bc07707383871d8b275bb0cc3cf441
Rainon <hrtechnicien.assistance.info@gmail.com>
2019-10-07 09:43:33 +0200

    commitherve

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 
 /*random*/
 let _key = "array";
-let _currentItems = ["salim", "Hervé", "Andrea","Hadibere", "MohammedK", "MohammedS","Sika", "Audrey", "Amel","Yannick"];
+
 function _shuffle(array) {
   for (i = array.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
