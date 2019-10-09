a1c91d8259f7ab249ba1d82905a80f99eadeb5cb
Rainon <hrtechnicien.assistance.info@gmail.com>
2019-10-09 12:46:24 +0200

    dernière version

--- javascript/script.js
+++ javascript/script.js
@@ -33,7 +33,7 @@ function clickRandom() {
     localStorage.setItem(_key, JSON.stringify(_currentItems));
     return _selectedItem;
   
-  })([`<p>Salim</p><img src="img/salim1.png ">`, `<p>Hervé</p><img src="img/herve1.png ">`, `<p>Andréa</p><img src="img/andrea1.png ">`,`<p>Hadibéré</p><img src="img/hadibere1.png ">`,`<p>Mohamed K</p><img src="img/mohamedK1.png ">`, `<p>Mohamed S</p><img src="img/mohameds1.png ">`,`<p>Sika</p><img src="img/sika1.png ">`,`<p>Audrey</p><img src="img/audrey1.png ">`, `<p> Amel</p><img src="img/amel1.png ">`,`<p>Yannick</p><img src="img/yannick1.png ">`]);
+  })([`<p>Salim</p><img src="img/salim1.png ">`, `<p>Hervé</p><img src="img/herve1.png ">`, `<p>Andréa</p><img src="img/andrea1.png ">`,`<p>Hadibéré</p><img src="img/hadibere1.png ">`,`<p>Mohamed K</p><img src="img/mohamedK1.png ">`, `<p>Mohamed S</p><img src="img/mohameds1.png ">`,`<p>Sika</p><img src="img/sika1.png ">`,`<p>Audrey</p><img src="img/audrey1.png ">`, `<p> Amel</p><img src="img/amel1.png ">`,`<p>Yannick</p><img src="img/yannick1.png ">`,`<p>Quentin</p><img src="img/quentin.png ">`]);
   
   
 /*trigger btn*/
@@ -88,6 +88,9 @@ document.getElementById("information").addEventListener("click", () => {
 document.getElementById("iconeinfo").addEventListener("click", () => {
   infoHtml();
 });
+document.getElementById("contactform").addEventListener("click", () => {
+  contactHtml();
+});
 
 /*list names
 let input = document.getElementById('name');
@@ -99,8 +102,8 @@ form.addEventListener("submit", function (e) {
 });*/
 
 function infoHtml(){
-const essai = document.querySelector('.test');
-essai.innerHTML =`<div class="grid-container">
+const infoList = document.querySelector('.test');
+infoList.innerHTML =`<div class="grid-container">
         <div class="left">
           <div id="info" class="titre">
             <h2> Ils sont dix et DÉTERMINÉS</h2>
@@ -119,25 +122,42 @@ essai.innerHTML =`<div class="grid-container">
         <div class="fo10"><img src="img/herve.png" alt="Hervé"><p>Hervé</p></div>
       </div>`;
 }
-
-// js pour contact
+function contactHtml(){
+  const formContact = document.querySelector('.test');
+  formContact.innerHTML =`<div class="grid-container2">
+  <div class="formaera">
+              <h2>Contactez-nous</h2>
+              <form action="https://formspree.io/unicorncat.r.us@gmail.com" method="POST">
+                <p><label>E-mail :</label>
+                  <input type="email" name="email" class="email" /></p>
+                <p><label> Sujet : </label>
+                  <input type="text" name="subject" class="subject"></p>
+                <p><label id="contact">Message : </label>
+                  <textarea name="message" class="message" cols="30" rows="3"></textarea></p>
+                <p><button type="submit" class="submit">Envoyer</button></p>
+              </form>
+            </div>
+            <div class="hand2">
+              <div class="laissez"><img src="img/main2.png" width="100%" height="auto" alt="main">
+              </div>
+            </div>
+          </div>
+</div>`;
+  }
 $(document).ready(function (){
   $('.submit').click(function (event) {
      console.log('Clicked button')
-
      var email = $('.email').val()
      var subject = $('.subject').val()
      var message = $('.message').val()
      var statusElm = $('.status')
      statusElm.empty()
-
      if(email.length > 5 && email.includes('@') && email.includes('.')) {
        statusElm.append('<div>Email is valid</div>')
   } else  {
         event.preventDefault()
         statusElm.append('<div>Email is not valid</div>')
   }
-
    if(subject.length >= 2) {
         statusElm.append('<div>Subject is valid</div>')
    } else {
